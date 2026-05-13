c4d67952798811e71d19e5f0cc577553cfc3d2a3
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2026-05-13 08:46:42 -0300

    feat: learning jinja and adding intermediate model using it's power

---
+++ models/intermediate/int_orders__pivoted.sql
@@ -0,0 +1,19 @@
+{% set payment_methods = ['debit_card', 'credit_card', 'gift_card', 'coupon', 'bank_transfer'] %}
+
+with payments as (
+    select *
+    from {{ ref('stg_stripe__payments') }}
+),
+
+pivoted as (
+    select 
+        order_id,
+        {% for method in payment_methods %}
+            sum(case when payment_method = '{{method}}' then amount else 0 end) as {{method}}_amount,
+        {%- endfor -%}
+    from payments 
+    group by 1
+    order by order_id
+)
+
+select * from pivoted
